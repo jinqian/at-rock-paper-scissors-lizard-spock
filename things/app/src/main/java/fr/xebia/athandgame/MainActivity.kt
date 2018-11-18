113d808b4e5ca5167c0bca79b5d76da68a7b57cd
Qian Jin <qian.jin.dev@gmail.com>
2018-11-18 15:26:24 +0100

    adjust game play

--- things/app/src/main/java/fr/xebia/athandgame/MainActivity.kt
+++ things/app/src/main/java/fr/xebia/athandgame/MainActivity.kt
@@ -27,16 +27,25 @@ class MainActivity : Activity() {
     private var isCountingDown = false
     private val gestureGenerator = GestureGenerator()
 
+    private var handUp = false
+    private var currentGesture: Gesture? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d(TAG, "Starting MainActivity")
 
         // I2C setup
         Log.d(TAG, "Setup I2C devices")
         val peripheralManager = PeripheralManager.getInstance()
         pwm = AdafruitPwm(I2C_DEVICE_NAME, I2C_SERVO_ADDRESS, true)
         pwm.setPwmFreq(60)
+
+        // calibrate servos to down angel
+        pwm.setPwm(Gesture.ROCK.driverPin, 0, SERVO_DOWN)
+        pwm.setPwm(Gesture.PAPER.driverPin, 0, SERVO_DOWN)
+        pwm.setPwm(Gesture.SCISSORS.driverPin, 0, SERVO_DOWN)
+        pwm.setPwm(Gesture.SPOCK.driverPin, 0, SERVO_DOWN)
+        pwm.setPwm(Gesture.LIZARD.driverPin, 0, SERVO_DOWN)
 
         Log.d(TAG, "Configuring GPIO pins")
         ledGpio = peripheralManager.openGpio(BoardDefaults.gpioForLED)
@@ -49,7 +58,7 @@ class MainActivity : Activity() {
             KeyEvent.KEYCODE_SPACE
         )
         buttonInputDriver.register()
-        countDownTimer = object : CountDownTimer(2000, 1000) {
+        countDownTimer = object : CountDownTimer(3000, 1000) {
 
             override fun onTick(millisUntilFinished: Long) {
                 isCountingDown = true
@@ -65,14 +74,19 @@ class MainActivity : Activity() {
                 mediaPlayer.start()
 
                 // TODO test servo motors should not block current thread
-                moveServo(gestureGenerator.fire())
+
             }
         }
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
         if (keyCode == KeyEvent.KEYCODE_SPACE) {
-            countDown()
+            if (handUp) {
+                reset()
+            } else {
+                fireGesture(gestureGenerator.fire())
+//                countDown()
+            }
             setLedValue(true)
             return true
         }
@@ -80,10 +94,18 @@ class MainActivity : Activity() {
         return super.onKeyDown(keyCode, event)
     }
 
-    private fun moveServo(gesture: Gesture) {
-        pwm.setPwm(gesture.driverPin, 0, SERVO_MIN)
-        Thread.sleep(200)
-        pwm.setPwm(gesture.driverPin, SERVO_MIN, 0)
+    private fun reset() {
+        handUp = false
+        currentGesture?.let {
+            pwm.setPwm(it.driverPin, 0, SERVO_DOWN)
+        }
+    }
+
+    private fun fireGesture(gesture: Gesture) {
+        handUp = true
+        currentGesture = gesture
+        pwm.setPwm(gesture.driverPin, 0, SERVO_HAND)
+//        pwm.setPwm(gesture.driverPin, SERVO_MIN, 0)
     }
 
     private fun countDown() {
@@ -127,7 +149,7 @@ class MainActivity : Activity() {
 
         private const val I2C_SERVO_ADDRESS: Int = 0x40
 
-        private const val SERVO_MIN = 150
-        private const val SERVO_MAX = 600
+        private const val SERVO_DOWN = 100
+        private const val SERVO_HAND = 350
     }
 }
--- things/app/src/main/java/fr/xebia/athandgame/game/Gesture.kt
+++ things/app/src/main/java/fr/xebia/athandgame/game/Gesture.kt
@@ -3,8 +3,8 @@ package fr.xebia.athandgame.game
 enum class Gesture(val id: Int, val driverPin: Int) {
     ROCK(0, 0),
     PAPER(1, 2),
-    SCISSORS(2, 4),
-    SPOCK(3, 6),
+    SCISSORS(2, 3),
+    SPOCK(3, 5),
     LIZARD(4, 7)
 }
 
